c005cd1fff1d62ae26a9e1de188948fbca5e5be9
HolographicTripwire <josephsmagen@gmail.com>
2025-07-01 20:19:50 +0100

    Drastically simplify the structure of PathWrapper

---
+++ src/paths/wrapper.rs
@@ -0,0 +1,14 @@
+use std::marker::PhantomData;
+
+use crate::paths::{Path, PathImpl};
+
+pub struct PathWrapper<L,R,P>(P, PhantomData<(L,R)>) where P: Path<L,R>;
+
+impl <L,R,P> PathWrapper<L,R,P> where P: Path<L,R> {
+    pub fn new(to_wrap: P) -> Self { Self(to_wrap, PhantomData) }
+    pub fn get_wrapped(&self) -> &P { &self.0 }
+}
+
+impl <L,R,P> Into<PathImpl<PathImpl<L,R>,()>> for PathWrapper<L,R,P> where P: Path<L,R>
+    { fn into(self) -> PathImpl<PathImpl<L,R>,()> { PathImpl::wrapper(self.0.into()) } }
+impl <L,R,P> Path<PathImpl<L,R>,()> for PathWrapper<L,R,P> where P: Path<L,R> {}

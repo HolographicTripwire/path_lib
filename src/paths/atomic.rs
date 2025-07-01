62249c8a880316756b844e05fb1414ad65e7a7e9
HolographicTripwire <josephsmagen@gmail.com>
2025-07-01 20:19:20 +0100

    Drastically simplify the structure of AtomicPath

--- src/paths/atomic.rs
+++ src/paths/atomic.rs
@@ -1,18 +1,12 @@
-use crate::paths::{inner::PathInner, Path, PathAtom, PathImpl, PathLeft, PathRight};
+use crate::paths::{Path, PathImpl, PathPrimitive};
 
-pub struct AtomicPath<Atom>(Atom);
+pub struct AtomicPath<Atom: PathPrimitive>(Atom);
 
-impl <Atom> AtomicPath<Atom> {
-    fn atom(&self) -> &Atom { &self.0 }
+impl <Atom: PathPrimitive> AtomicPath<Atom> {
+    pub fn new(atom: Atom) -> Self { Self(atom) }
+    pub fn atom(&self) -> &Atom { &self.0 }
 }
 
-impl <A,L,R> Path<A,L,R> for AtomicPath<A> {}
-
-impl <A,LA,LL,LR,L,RA,RL,RR,R> PathInner<A,LA,LL,LR,L,RA,RL,RR,R> for
-AtomicPath<A> where
-L: Path<LA,LL,LR>, LA: PathAtom<LL,LR,L>, LL: PathLeft<LA,LR,L>, LR: PathRight<LA,LL,L>,
-R: Path<RA,RL,RR>, RA: PathAtom<RL,RR,R>, RL: PathLeft<RA,RR,R>, RR: PathRight<RA,RL,R> {
-    fn as_impl<'a,>(&'a self) -> PathImpl<'a,A,LA,LL,LR,L,RA,RL,RR,R> {
-        super::PathImpl::Atomic(self)
-    }
-}
+impl <A: PathPrimitive> Into<PathImpl<A,()>> for AtomicPath<A>
+    { fn into(self) -> PathImpl<A,()> { PathImpl::atom(self.0) } }
+impl <A: PathPrimitive> Path<A,()> for AtomicPath<A> {}

712c6f062fcb03bf98c7efd6f0448e90219130fe
HolographicTripwire <josephsmagen@gmail.com>
2025-07-01 20:19:31 +0100

    Drastically simplify the structure of PathJoiner

--- src/paths/joiner.rs
+++ src/paths/joiner.rs
@@ -1,30 +1,26 @@
 use std::marker::PhantomData;
 
-use crate::paths::{inner::PathInner, Path, PathAtom, PathImpl, PathLeft, PathRight};
+use crate::paths::{Path, PathImpl};
 
-pub struct PathJoiner<LA,LL,LR,L,RA,RL,RR,R>
-(L, R, PhantomData<(LA,LL,LR,RA,RL,RR)>) where
-L: Path<LA,LL,LR>, LA: PathAtom<LL,LR,L>, LL: PathLeft<LA,LR,L>, LR: PathRight<LA,LL,L>,
-R: Path<RA,RL,RR>, RA: PathAtom<RL,RR,R>, RL: PathLeft<RA,RR,R>, RR: PathRight<RA,RL,R>;
+pub struct PathJoiner<LL,L,RL,R>
+(L,R,PhantomData<(LL,RL)>) where
+L: Path<LL,()>, 
+R: Path<RL,()>; 
 
-impl <LA,LL,LR,L,RA,RL,RR,R>
-PathJoiner<LA,LL,LR,L,RA,RL,RR,R> where
-L: Path<LA,LL,LR>, LA: PathAtom<LL,LR,L>, LL: PathLeft<LA,LR,L>, LR: PathRight<LA,LL,L>,
-R: Path<RA,RL,RR>, RA: PathAtom<RL,RR,R>, RL: PathLeft<RA,RR,R>, RR: PathRight<RA,RL,R> {
+impl <LL,L,RL,R> PathJoiner<LL,L,RL,R> where
+L: Path<LL,()>, 
+R: Path<RL,()> {
+    pub fn new(left: L, right: R) -> Self { Self(left, right, PhantomData) }
     pub fn left(&self) -> &L { &self.0 }
     pub fn right(&self) -> &R { &self.1 }
 }
 
-impl <A,LA,LL,LR,L,RA,RL,RR,R> Path<A,L,R> for
-PathJoiner<LA,LL,LR,L,RA,RL,RR,R> where
-L: Path<LA,LL,LR>, LA: PathAtom<LL,LR,L>, LL: PathLeft<LA,LR,L>, LR: PathRight<LA,LL,L>,
-R: Path<RA,RL,RR>, RA: PathAtom<RL,RR,R>, RL: PathLeft<RA,RR,R>, RR: PathRight<RA,RL,R> {}
-
-impl <A,LA,LL,LR,L,RA,RL,RR,R> PathInner<A,LA,LL,LR,L,RA,RL,RR,R> for
-PathJoiner<LA,LL,LR,L,RA,RL,RR,R> where
-L: Path<LA,LL,LR>, LA: PathAtom<LL,LR,L>, LL: PathLeft<LA,LR,L>, LR: PathRight<LA,LL,L>,
-R: Path<RA,RL,RR>, RA: PathAtom<RL,RR,R>, RL: PathLeft<RA,RR,R>, RR: PathRight<RA,RL,R> {
-    fn as_impl<'a,>(&'a self) -> PathImpl<'a,A,LA,LL,LR,L,RA,RL,RR,R> {
-        super::PathImpl::Joiner(self)
-    }
+impl <LL,L,RL,R> Into<PathImpl<LL,RL>> for PathJoiner<LL,L,RL,R> where
+L: Path<LL,()>, 
+R: Path<RL,()> { 
+    fn into(self) -> PathImpl<LL,RL> { PathImpl::joiner(self.0.into(), self.1.into()) }
 }
+
+impl <LL,L,RL,R> Path<LL,RL> for PathJoiner<LL,L,RL,R> where
+L: Path<LL,()>, 
+R: Path<RL,()> {}

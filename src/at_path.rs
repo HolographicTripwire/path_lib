871dd77b0a5fbcb9b5e973bff41be74d8b403a72
HolographicTripwire <josephsmagen@gmail.com>
2025-07-03 15:11:53 +0100

    Minor updates to AtPath

--- src/at_path.rs
+++ src/at_path.rs
@@ -1,20 +1,20 @@
 use std::marker::PhantomData;
 
-use crate::{has_children::HasDescendants, paths::Path};
+use crate::{has_children::HasDescendants, paths::{JoinablePath, Path}};
 
-pub struct AtPath<'a,To,P,Left,Right,Joiner> where
-Left: Clone, Right: Clone, P: Path<Left,Right> {
+pub struct AtPath<'a,To,PathTo,Left,Right> where
+Left: Clone, Right: Clone, PathTo: Path<Left,Right> {
     obj: &'a To,
-    path: P,
-    phantom: PhantomData<(Left,Right,Joiner)>,
+    path: PathTo,
+    phantom: PhantomData<(Left,Right)>,
 }
-impl <'a,T,P,L,R,J> AtPath<'a,T,P,L,R,J> where
-L: Clone, R: Clone, P: Path<L,R> {
-    pub fn from_at(obj_at: &'a T, path: P) -> Self { Self { obj: obj_at, path, phantom: PhantomData }}
-    pub fn from_in<O: HasDescendants<'a,L,R,P,J,T>>(obj_in: &'a O, path: P) -> Result<Self,()> {
+impl <'a,To,PathTo,Left,Right> AtPath<'a,To,PathTo,Left,Right> where
+Left: Clone, Right: Clone, PathTo: Path<Left,Right> {
+    pub fn from_at(obj_at: &'a To, path: PathTo) -> Self { Self { obj: obj_at, path, phantom: PhantomData }}
+    pub fn from_in<Joiner,O: HasDescendants<'a,Left,Right,PathTo,Joiner,To>>(obj_in: &'a O, path: PathTo) -> Result<Self,()> {
         Ok(Self::from_at(obj_in.get_descendant(&path)?,path))
     }
 
-    pub fn obj(&self) -> &'a T { &self.obj }
-    pub fn path(&'a self) -> &'a P { &self.path } 
+    pub fn obj(&'a self) -> &'a To { &self.obj }
+    pub fn path(&'a self) -> &'a PathTo { &self.path } 
 }

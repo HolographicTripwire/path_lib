ee9b4b5a27052077f894cae2f000579e31efaa97
HolographicTripwire <josephsmagen@gmail.com>
2025-07-03 15:12:24 +0100

    Introduce AtJoinablePath

--- src/at_path.rs
+++ src/at_path.rs
@@ -18,3 +18,31 @@ Left: Clone, Right: Clone, PathTo: Path<Left,Right> {
     pub fn obj(&'a self) -> &'a To { &self.obj }
     pub fn path(&'a self) -> &'a PathTo { &self.path } 
 }
+
+pub trait AtJoinablePath<'a,J,
+OldTo: 'a + HasDescendants<'a,PathJoinerL,PathJoinerR,PathJoiner,J,NewTo>,
+NewTo: 'a,
+OldPathToL: Clone, OldPathToR: Clone, OldPathTo: 'a + JoinablePath<OldPathToL,OldPathToR,PathJoinerL,PathJoinerR,PathJoiner,OL=NewPathToL,OR=NewPathToR,Output=NewPathTo>,
+PathJoinerL: Clone, PathJoinerR: Clone, PathJoiner: Path<PathJoinerL,PathJoinerR>,
+NewPathToL: Clone, NewPathToR: Clone, NewPathTo: 'a + Path<NewPathToL,NewPathToR>> {
+    fn _obj(&'a self) -> &'a OldTo;
+    fn _path(&'a self) -> &'a OldPathTo;
+
+    fn join(&'a self, subpath: PathJoiner) -> Result<AtPath<'a,NewTo,NewPathTo,NewPathToL,NewPathToR>,()> {
+        let obj = self._obj().get_descendant(&subpath)?;
+        let path = self._path().clone().join(subpath);
+        Ok(AtPath::from_at(obj,path))
+    }
+}
+
+impl <'a,J,
+OldTo: 'a + HasDescendants<'a,PathJoinerL,PathJoinerR,PathJoiner,J,NewTo>,
+NewTo: 'a,
+OldPathToL: Clone, OldPathToR: Clone, OldPathTo: 'a + JoinablePath<OldPathToL,OldPathToR,PathJoinerL,PathJoinerR,PathJoiner,OL=NewPathToL,OR=NewPathToR,Output=NewPathTo>,
+PathJoinerL: Clone, PathJoinerR: Clone, PathJoiner: Path<PathJoinerL,PathJoinerR>,
+NewPathToL: Clone, NewPathToR: Clone, NewPathTo: 'a + Path<NewPathToL,NewPathToR>> 
+AtJoinablePath<'a,J,OldTo,NewTo,OldPathToL,OldPathToR,OldPathTo,PathJoinerL,PathJoinerR,PathJoiner,NewPathToL,NewPathToR,NewPathTo> for
+AtPath<'a,OldTo,OldPathTo,OldPathToL,OldPathToR> {
+    fn _obj(&'a self) -> &'a OldTo { self.obj() }
+    fn _path(&'a self) -> &'a OldPathTo { self.path() }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ mod at_path;
 
 pub use has_children::HasChildren;
 pub use paths::{Path,PathImpl};
-pub use at_path::AtPath;
+pub use at_path::{AtPath,AtJoinablePath};

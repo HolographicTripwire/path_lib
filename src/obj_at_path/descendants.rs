379f0c9cb69ea8ec69afd27f22f9dc0aab83c837
HolographicTripwire <josephsmagen@gmail.com>
2025-10-10 18:48:19 +0100

    Fix method naming on ObjAtPathWithDescendants

--- src/obj_at_path/descendants.rs
+++ src/obj_at_path/descendants.rs
@@ -1,6 +1,6 @@
 use crate::{obj_at_path::{ObjAtAppendablePath, ObjAtPath}, paths::{PathPair, PathPrimitive}, HasChildren, HasDescendants, Path};
 
-pub trait ObjAtPathWithChildren<'a,OldObj,Child,OldAtPath,Joiner,NewAtPath,Output>: ObjAtAppendablePath<'a, (), OldObj, Child, OldAtPath, Joiner> where
+pub trait ObjAtPathWithChildren<'a,OldObj,Child,OldAtPath,Joiner>: ObjAtAppendablePath<'a, (), OldObj, Child, OldAtPath, Joiner> where
 Child: 'a + PartialEq,
 OldObj:'a + HasChildren<'a,Joiner,Child>,
 OldAtPath:'a + Path,
@@ -25,39 +25,39 @@ Joiner:'a + PathPrimitive {
     }
 }
 
-impl <'a,OldObj,Child,OldAtPath,Joiner,NewAtPath,Output,SelfType> ObjAtPathWithChildren<'a,OldObj,Child,OldAtPath,Joiner,NewAtPath,Output> for SelfType where
+impl <'a,OldObj,Child,OldAtPath,Joiner,SelfType> ObjAtPathWithChildren<'a,OldObj,Child,OldAtPath,Joiner> for SelfType where
 Child: 'a + PartialEq,
 OldObj:'a + HasChildren<'a,Joiner,Child>,
 OldAtPath:'a + Path,
 Joiner:'a + PathPrimitive,
 SelfType: ObjAtAppendablePath<'a, (), OldObj, Child, OldAtPath, Joiner> {}
 
-pub trait ObjAtPathWithDescendants<'a,J,OldObj,Descendant,OldAtPath,Joiner,NewAtPath,Output>: ObjAtAppendablePath<'a, J, OldObj, Descendant, OldAtPath, Joiner> where
+pub trait ObjAtPathWithDescendants<'a,J,OldObj,Descendant,OldAtPath,Joiner>: ObjAtAppendablePath<'a, J, OldObj, Descendant, OldAtPath, Joiner> where
 Descendant: 'a + PartialEq,
 OldObj:'a + HasDescendants<'a,Joiner,J,Descendant>,
 OldAtPath:'a + Path,
 Joiner:'a + Path {
-    fn valid_primitive_paths(&'a self) -> impl IntoIterator<Item = Joiner> { self._obj().valid_paths() }
-    fn get_child(&'a self, path: &Joiner) -> Result<&'a Descendant,()> { self._obj().get_descendant(path) }
+    fn valid_paths(&'a self) -> impl IntoIterator<Item = Joiner> { self._obj().valid_paths() }
+    fn get_descendant(&'a self, path: &Joiner) -> Result<&'a Descendant,()> { self._obj().get_descendant(path) }
     fn get_located_descendant(&'a self, path: Joiner) -> Result<ObjAtPath<'a,Descendant,PathPair<OldAtPath,Joiner>>,()> {
         let child = self._obj().get_descendant(&path)?;
         let new_path = self._path().clone().pair_append(path);
         Ok(ObjAtPath::from_at(child, new_path))
     }
     
-    fn get_descendant(&'a self) -> impl IntoIterator<Item = &'a Descendant> { 
-        self.valid_primitive_paths()
+    fn get_descendants(&'a self) -> impl IntoIterator<Item = &'a Descendant> { 
+        self.valid_paths()
             .into_iter()
-            .map(|path| self.get_child(&path).expect("valid_primitive_paths returned an invalid path"))
+            .map(|path| self.get_descendant(&path).expect("valid_paths returned an invalid path"))
     }
     fn get_located_descendants(&'a self) -> impl IntoIterator<Item = ObjAtPath<'a,Descendant,PathPair<OldAtPath,Joiner>>> {
-        self.valid_primitive_paths()
+        self.valid_paths()
             .into_iter()
-            .map(|path| self.get_located_descendant(path).expect("valid_primitive_paths returned an invalid path"))
+            .map(|path| self.get_located_descendant(path).expect("valid_paths returned an invalid path"))
     }
 }
 
-impl <'a,J,OldObj,Descendant,OldAtPath,Joiner,NewAtPath,Output,SelfType> ObjAtPathWithDescendants<'a,J,OldObj,Descendant,OldAtPath,Joiner,NewAtPath,Output> for SelfType where
+impl <'a,J,OldObj,Descendant,OldAtPath,Joiner,SelfType> ObjAtPathWithDescendants<'a,J,OldObj,Descendant,OldAtPath,Joiner> for SelfType where
 Descendant: 'a + PartialEq,
 OldObj:'a + HasDescendants<'a,Joiner,J,Descendant>,
 OldAtPath:'a + Path,
